6e76649872411415c0ee27dd521c2946e284aa38
Tiago Seibel <tiagoseibel@gmail.com>
2018-10-22 14:49:34 -0300

    add project aula-web-10

---
+++ aula-web-10/src/main/java/beans/DepartamentoBean.java
@@ -0,0 +1,58 @@
+package beans;
+
+import dao.DepartamentoDAO;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import model.Departamento;
+
+@Named(value = "departamentoBean")
+@SessionScoped
+public class DepartamentoBean implements Serializable {
+
+    public DepartamentoBean() {
+    }
+    
+    public void insertAction() {
+        departamentoDAO.insert( departamento );
+        this.departamento = new Departamento();
+        this.departamentos = departamentoDAO.findAll();
+    }
+    
+    public void atualizarAction() {
+        this.departamentos = departamentoDAO.findAll();
+    }
+    
+    public void removeAction(Departamento dpt) {
+        departamentoDAO.delete(dpt);
+        this.departamentos = departamentoDAO.findAll();
+    }
+    
+    private DepartamentoDAO departamentoDAO = new DepartamentoDAO();
+    
+    private Departamento departamento = new Departamento();
+    private List<Departamento> departamentos = new ArrayList<>();
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<Departamento> getDepartamentos() {
+        if (this.departamentos == null) {
+            this.departamentos = departamentoDAO.findAll();
+        }
+        return departamentos;
+    }
+
+    public void setDepartamentos(List<Departamento> departamentos) {
+        this.departamentos = departamentos;
+    }
+    
+    
+}
---
+++ aula-web-10/src/main/java/dao/DepartamentoDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import config.HibernateUtil;
+import java.util.List;
+import model.Departamento;
+import org.hibernate.Session;
+
+public class DepartamentoDAO {
+    
+    private Session session;
+    
+    public DepartamentoDAO() {
+        session = HibernateUtil.getSessionFactory().openSession();
+    }
+    
+    public void insert(Departamento departamento) {
+        session.getTransaction().begin();
+        session.save(departamento);
+        session.getTransaction().commit();
+    }
+    
+    public void delete(Departamento departamento) {
+        session.getTransaction().begin();
+        session.remove(departamento);
+        session.getTransaction().commit();
+    }
+    
+    public List<Departamento> findAll() {
+        return session.createQuery("select d from Departamento d").list();
+    }
+}
